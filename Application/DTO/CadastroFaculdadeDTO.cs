155a314f1591cba5fac2859da8e3fc9753d1bb7b
Gilberto Barbosa <gilbertoaraujo229@gmail.com>
2023-11-18 18:59:07 -0300

    FIX: Remove dto's

--- Application/DTO/CadastroFaculdadeDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Application.DTO
-{
-    public class CadastroFaculdadeDTO
-    {
-        public string Nome { get; set; }
-        public string Dominio { get; set; }
-    }
-}
--- Application/DTO/CadastroUsuarioDTO.cs
+++ Application/DTO/CadastroUsuarioDTO.cs
@@ -2,8 +2,6 @@
 {
     public class CadastroUsuarioDTO
     {
-        public string Nome { get; set; }
-        public string Sobrenome { get; set; }
         public string Email { get; set; }
         public DateTime DataNascimento { get; set; }
         public string Password { get; set; }
